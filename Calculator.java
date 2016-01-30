a2d49bfb713c99558495860e56f07f3f83814044
Ava Kocher <akocher@Avas-MacBook-Pro.local>
2016-01-30 09:48:04 +0100

    adding two numbers

--- Calculator.java
+++ Calculator.java
@@ -2,9 +2,14 @@ import java.util.*;
 public class Calculator {
 		public static void main (String [] args){
 			Scanner scanner = new Scanner(System.in);
-			System.out.print( " Bitte eingeben [ +-*/] zahl1 zahl2");
+			System.out.print( " Bitte eingeben [ +-*/] zahl1 zahl2 ");
 			String Operator = scanner.next();
 			int zahl1=scanner.nextInt();
 			int zahl2=scanner.nextInt();
+			int result = 0;
+			if (Operator.equals("+")){
+				result = zahl1 + zahl2;
+			}
+			System.out.println(zahl1 + " " + Operator + " " + zahl2 + " = " + result);
 		}
 }
